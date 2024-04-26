fa371ba2d981c885ad1892dcb404b0d92d6b1064
maks-yakauleu <max.yakauleu@devol.network>
2024-04-26 12:00:19 +0200

    make modal not exportable

--- ts_src/dart_web3modal_adapter.ts
+++ ts_src/dart_web3modal_adapter.ts
@@ -17,9 +17,10 @@ const solanaDevnet = {
 
 const chains = [solanaDevnet];
 
+// 96fa9cbf333cf05f246ae5cb5afd7239
 const projectId = "d23bfefd133ecce1f6d2ed7b52bbd5b4";
 
-export const modal = createWeb3Modal({
+const modal = createWeb3Modal({
     chains: chains,
     projectId: projectId,
     solanaConfig: { metadata: metadata },
