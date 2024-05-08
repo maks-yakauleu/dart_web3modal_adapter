8683090264b9fb4fa2398574b8b16490b467a74b
maks-yakauleu <max.yakauleu@devol.network>
2024-05-08 18:29:47 +0200

    add log

--- ts_src/dart_web3modal_adapter.ts
+++ ts_src/dart_web3modal_adapter.ts
@@ -61,6 +61,7 @@ export function getPublicKey() : PublicKey {
 export function signTransaction(transaction: Transaction) : Promise<Uint8Array>{
     const provider = modal.getWalletProvider() as Provider;
     let signaturePromise = provider.signTransaction(transaction).then((res)=>{
+        console.log("res");
         console.log(res);
         console.log(res.signatures[0]);
         console.log(res.signatures[0].signature);
